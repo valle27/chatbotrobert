7daedd21134434f9ef7fc9d6126b833b5751f27a
hector <hectoralfonsovalle@protonmail.com>
2024-05-10 08:14:10 -0600

    Version 1.0, program created, asking for name

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+author: hvalle
+date : May/10/2024
+*/
+
+
+int main() {
+	//we say hello
+	printf("Hello, my name is Robert\n");
+	printf("What is your name? ");
+	char name[30];
+	scanf("%s", name);
+
+
+
+}
